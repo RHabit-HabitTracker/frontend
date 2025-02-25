6665d55bd2d20f29a488514320ab864a3dc41003
ShadowWolf <leon.scharf@gmail.com>
2025-02-25 17:46:15 +0100

    Started with Footer, Links dont work yet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PlaceholderSiteComponent } from './placeholder-site/placeholder-site.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
+  //{ path: 'placeholder-site', component: PlaceholderSiteComponent}
 ];
---
+++ src/app/placeholder-site/placeholder-site.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder-site',
+  imports: [],
+  templateUrl: './placeholder-site.component.html',
+  styleUrl: './placeholder-site.component.css'
+})
+export class PlaceholderSiteComponent {
+
+}
