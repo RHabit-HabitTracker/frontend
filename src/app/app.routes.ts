0d51d2c9ce6290fd881d2dd605cdbe5a67d5c99b
MBecker7 <marcbecker991@web.de>
2025-03-02 21:14:44 +0100

    Updated default route to use loginpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { PlaceholderSiteComponent } from './pages/placeholder-site/placeholder-site.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: LoginComponent },
   { path: 'placeholder-site', component: PlaceholderSiteComponent},
   { path: 'login', component: LoginComponent},
 ];
