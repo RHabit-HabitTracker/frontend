ac2160042a071ec2ec61a95a20b681ade7982294
MBecker7 <marcbecker991@web.de>
2025-03-02 21:04:59 +0100

    Add register form card with validation

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [FloatLabelModule, InputTextModule, FormsModule, ButtonModule, CardModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
+  registerEmail: string | undefined;
+  registerPassword: string | undefined;
 
+  onSubmit() {
+    
+    console.log('Form Submitted:');
+    console.log('Email:',this.registerEmail);
+    console.log('Password:',this.registerPassword);
+      
+  }
 }
