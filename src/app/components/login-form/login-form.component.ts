f03c962bdc8be54c082c2a863e3c93e91fea2687
MBecker7 <marcbecker991@web.de>
2025-03-02 13:33:22 +0100

    Add login form component

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { InputTextModule } from 'primeng/inputtext';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [FloatLabelModule, InputTextModule, FormsModule, ButtonModule, CardModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
 
+    loginEmail: string | undefined;
+
+    loginPassword: string | undefined;
+
+
+  onSubmit() {
+    
+    console.log('Form Submitted:');
+    console.log('Email:',this.loginEmail);
+    console.log('Password:',this.loginPassword);
+      
+  }
 }
