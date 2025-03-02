31dac3bab7909ab58d51445e934f60cb3c13048d
MBecker7 <marcbecker991@web.de>
2025-03-02 21:32:00 +0100

    Add register and login form to login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../../components/footer/footer.component';
-import { HeaderComponent } from "../../components/header/header.component";
+import { RegisterFormComponent } from "../../components/register-form/register-form.component";
+import { LoginFormComponent } from "../../components/login-form/login-form.component";
+import { SplitterModule } from 'primeng/splitter';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RegisterFormComponent, LoginFormComponent, SplitterModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
