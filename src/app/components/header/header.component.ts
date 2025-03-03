98e775231ee93cc1db69b2917b2f3f2c351ce54e
ShadowWolf <leon.scharf@gmail.com>
2025-03-03 11:20:58 +0100

    Change Header from Manual HTML & CSS to Primeng
    Remove old HTML
    Add new Header made with PrimeNG

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+import { CommonModule } from '@angular/common';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormsModule } from '@angular/forms';
+import { style } from '@angular/animations';
+//import { AuthService } from './auth.service';  // Import the AuthService, needs login logic
+
+
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [CommonModule,
+            Menubar,
+            InputSwitchModule,
+            FormsModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+    isDarkMode: boolean = false;
+
+    items = [
+        { label: '', icon: 'pi pi-ellipsis-v', items: [
+            { label: 'Contact', icon: 'pi, pi-envelope', routerLink: '/contact'},
+            { label: 'About Us', icon: 'pi pi-info-circle', routerLink: '/landing'},
+            { label: 'Impressum', icon: 'pi pi-book', routerLink: '/imprint'},
+            { label: 'Terms of Service', icon: 'pi pi-paperclip', routerLink: '/terms-of-service'},
+            { label: 'Privacy policy', icon: 'pi pi-cloud', routerLink: '/privacy-policy'},
+        ] },
+        { label: 'Login', icon: 'pi pi-user', routerLink: '/login', visible: true },
+    ];
+
+    ngOnInit() {
+        this.isDarkMode = localStorage.getItem('darkMode') === 'true';
+        this.applyTheme();
+    }
+
+    toggleTheme() {
+        this.isDarkMode = !this.isDarkMode;
+        localStorage.setItem('darkMode', String(this.isDarkMode));
+        this.applyTheme();
+    }
+
+    applyTheme() {
+        const themeLink = document.getElementById('theme-css') as HTMLLinkElement;
+        if (this.isDarkMode) {
+        themeLink.href = 'assets/themes/lara-dark-blue/theme.css';
+        } else {
+        themeLink.href = 'assets/themes/lara-light-blue/theme.css';
+        }
+    }
 }
