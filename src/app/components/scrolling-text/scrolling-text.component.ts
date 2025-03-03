fc5e096097c9851e6bf7c1c3b9f45d63cc931b05
hucet <cetin.hueseyin@outlook.de>
2025-03-03 16:38:04 +0100

    Generate scrolling-text component
    TG-50

---
+++ src/app/components/scrolling-text/scrolling-text.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scrolling-text',
+  imports: [],
+  templateUrl: './scrolling-text.component.html',
+  styleUrl: './scrolling-text.component.css'
+})
+export class ScrollingTextComponent {
+
+}
