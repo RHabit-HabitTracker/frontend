1018d5f5b39118d5250f3baa7d8d3c44a284e57e
hucet <cetin.hueseyin@outlook.de>
2025-03-04 16:41:22 +0100

    Edit landing.component.html and import PrimeNG components

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { ScrollingTextComponent } from '../../components/scrolling-text/scrolling-text.component';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { BadgeModule } from 'primeng/badge';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 @Component({
   selector: 'app-landing',
-  imports: [ScrollingTextComponent],
+  imports: [ScrollingTextComponent, ButtonModule, MenubarModule, InputTextModule, CardModule, BadgeModule, SplitButtonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
