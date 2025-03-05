50d91e00c1b0760d743803fb104b817eb1eb83f3
ShadowWolf <leon.scharf@gmail.com>
2025-03-05 15:43:19 +0100

    add new header
    header transformed from menubar to toolbar
    includes menubar, switch and logo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
-import { Toolbar } from 'primeng/toolbar';
-import { AvatarModule } from 'primeng/avatar';
-import { ButtonModule } from 'primeng/button';
+import { Router, RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+import { CommonModule } from '@angular/common';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormsModule } from '@angular/forms';
+import { style } from '@angular/animations';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { ToolbarModule } from 'primeng/toolbar';
+//import { AuthService } from './auth.service';  // Import the AuthService, needs login logic
+
+
 
 @Component({
   selector: 'app-header',
-  imports: [Toolbar, AvatarModule, ButtonModule],
+  imports: [CommonModule,
+            Menubar,
+            InputSwitchModule,
+            FormsModule,
+            ToggleSwitchModule,
+            ToolbarModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+    isDarkMode: boolean = false;
+
+    items = [
+        { label: '', icon: 'pi pi-ellipsis-v', items: [
+            { label: 'Contact', icon: 'pi pi-envelope', routerLink: '/contact'},
+            { label: 'About Us', icon: 'pi pi-info-circle', routerLink: '/landing'},
+            { label: 'Impressum', icon: 'pi pi-book', routerLink: '/imprint'},
+            { label: 'Terms of Service', icon: 'pi pi-paperclip', routerLink: '/terms-of-service'},
+            { label: 'Privacy policy', icon: 'pi pi-cloud', routerLink: '/privacy-policy'},
+        ], /*style: { 'min-width': '120px', 'text-align': 'center' } */},
+        { label: 'Login', icon: 'pi pi-user', routerLink: '/login', visible: true, style: { 'background-color': 'pink' },},
+        
+    ];
+
+    ngOnInit() {
+    }
+
+    toggleTheme() {
+    }
+
+    applyTheme() {
+    }
 }
