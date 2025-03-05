aeb2d3ad5c4fa85ca2e27df4ec0380f72fe56ee2
ShadowWolf <leon.scharf@gmail.com>
2025-03-05 20:32:23 +0100

    Add page for Password Reset
    Reused Code from Login-form
    Changed only varable names and Displayed text

---
+++ src/app/pages/password-reset/password-reset.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ResetFormComponent } from "../../components/reset-form/reset-form.component";
+
+@Component({
+  selector: 'app-password-reset',
+  imports: [ResetFormComponent],
+  templateUrl: './password-reset.component.html',
+  styleUrl: './password-reset.component.css'
+})
+export class PasswordResetComponent {
+
+}
