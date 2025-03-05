ba8828c91ed9802aa71d5a7b3d1346e4090eead5
MBecker7 <marcbecker991@web.de>
2025-03-05 19:51:52 +0100

    Add responsiveness to login and register form

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FloatLabelModule } from "primeng/floatlabel"
+import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
@@ -9,22 +9,26 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
-  imports: [FloatLabelModule, InputTextModule, FormsModule, ButtonModule, CardModule, DividerModule, RouterLink],
+  imports: [
+    FloatLabelModule,
+    InputTextModule,
+    FormsModule,
+    ButtonModule,
+    CardModule,
+    DividerModule,
+    RouterLink,
+  ],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
+  loginEmail: string | undefined;
 
-    loginEmail: string | undefined;
-
-    loginPassword: string | undefined;
-
+  loginPassword: string | undefined;
 
   onSubmit() {
-    
     console.log('Form Submitted:');
-    console.log('Email:',this.loginEmail);
-    console.log('Password:',this.loginPassword);
-      
+    console.log('Email:', this.loginEmail);
+    console.log('Password:', this.loginPassword);
   }
 }
