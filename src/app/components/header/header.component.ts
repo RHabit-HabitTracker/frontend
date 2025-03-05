9f3da873e944098e9c8811b84bfa56ba2eddf831
MBecker7 <marcbecker991@web.de>
2025-03-05 17:09:04 +0100

    First working iteration of Navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { Toolbar } from 'primeng/toolbar';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
 import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [Toolbar, AvatarModule, ButtonModule],
+  imports: [
+    Menubar,
+    BadgeModule,
+    AvatarModule,
+    InputTextModule,
+    CommonModule,
+    RouterLink,
+    ButtonModule,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  items: MenuItem[] | undefined;
 
+  ngOnInit(): void {
+    this.items = [
+      { label: 'Startseite', icon: 'pi pi-envelope', routerLink: '/landing' },
+      { label: 'Kontakt', icon: 'pi pi-envelope', routerLink: '/contact' },
+      { label: 'Über uns', icon: 'pi pi-info-circle', routerLink: '/about' },
+    ];
+  }
 }
