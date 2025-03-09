df5f013d7eba67841ffcdb940f7794e1503f8d79
nile <nico.l.05.04.03@gmail.com>
2025-03-09 19:51:45 +0100

    Reformat header code

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,41 +10,47 @@ import { ToggleSwitchModule } from 'primeng/toggleswitch';
 import { ToolbarModule } from 'primeng/toolbar';
 //import { AuthService } from './auth.service';  // Import the AuthService, needs login logic
 
-
-
 @Component({
   selector: 'app-header',
-  imports: [CommonModule,
-            Menubar,
-            InputSwitchModule,
-            FormsModule,
-            ToggleSwitchModule,
-            ToolbarModule,
+  imports: [
+    CommonModule,
+    Menubar,
+    InputSwitchModule,
+    FormsModule,
+    ToggleSwitchModule,
+    ToolbarModule,
   ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
+  isDarkMode: boolean = false;
 
-    isDarkMode: boolean = false;
+  items = [
+    {
+      label: '',
+      icon: 'pi pi-ellipsis-v',
+      items: [
+        { label: 'Contact', icon: 'pi pi-envelope', routerLink: '/contact' },
+        { label: 'Impressum', icon: 'pi pi-book', routerLink: '/imprint' },
+        {
+          label: 'Terms of Service',
+          icon: 'pi pi-paperclip',
+          routerLink: '/terms-of-service',
+        },
+        {
+          label: 'Privacy policy',
+          icon: 'pi pi-cloud',
+          routerLink: '/privacy-policy',
+        },
+      ] /*style: { 'min-width': '120px', 'text-align': 'center' } */,
+    },
+    { label: 'Login', icon: 'pi pi-user', routerLink: '/login', visible: true },
+  ];
 
-    items = [
-        { label: '', icon: 'pi pi-ellipsis-v', items: [
-            { label: 'Contact', icon: 'pi pi-envelope', routerLink: '/contact'},
-            { label: 'Impressum', icon: 'pi pi-book', routerLink: '/imprint'},
-            { label: 'Terms of Service', icon: 'pi pi-paperclip', routerLink: '/terms-of-service'},
-            { label: 'Privacy policy', icon: 'pi pi-cloud', routerLink: '/privacy-policy'},
-        ], /*style: { 'min-width': '120px', 'text-align': 'center' } */},
-        { label: 'Login', icon: 'pi pi-user', routerLink: '/login', visible: true,},
-        
-    ];
+  ngOnInit() {}
 
-    ngOnInit() {
-    }
+  toggleTheme() {}
 
-    toggleTheme() {
-    }
-
-    applyTheme() {
-    }
+  applyTheme() {}
 }
