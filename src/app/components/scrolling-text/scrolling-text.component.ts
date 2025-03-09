e9f31bd31b2616f8b384c3979066c26f7c9a9eeb
hucet <cetin.hueseyin@outlook.de>
2025-03-09 01:39:37 +0100

    Edit scrolling-text component

--- src/app/components/scrolling-text/scrolling-text.component.ts
+++ src/app/components/scrolling-text/scrolling-text.component.ts
@@ -1,21 +1,53 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgxFastMarqueeModule } from 'ngx-fast-marquee';
-import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
+import { AvatarModule } from 'primeng/avatar';
+
+interface Quote {
+  image: string;
+  author: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-scrolling-text',
-  imports: [NgxFastMarqueeModule, CardModule, FieldsetModule],
+  imports: [CommonModule, NgxFastMarqueeModule, FieldsetModule, AvatarModule],
   templateUrl: './scrolling-text.component.html',
   styleUrl: './scrolling-text.component.css'
 })
-export class ScrollingTextComponent implements OnInit {
-  // Renderer2 for DOM manipulation
-  constructor(private renderer: Renderer2) {}
 
-  ngOnInit() {
-    const randomStart = Math.random();
-    // CSS variable for random start
-    this.renderer.setProperty(document.documentElement, 'style', "--random-start: " + randomStart);
-  }
+export class ScrollingTextComponent {
+  quotes: Quote[] = [
+    {
+      image: 'assets/img-quotes/ghandi.jpg',
+      author: 'Mahatma Ghandi',
+      text: 'Sei du selbst die Veränderung, die du dir wünschst für diese Welt.'
+    },
+    {
+      image: 'assets/img-quotes/hill.jpg',
+      author: 'Napoleon Hill',
+      text: 'Warte nicht. Die Zeit wird niemals genau richtig sein.'
+    },
+    {
+      image: 'assets/img-quotes/keller.jpg',
+      author: 'Helen Keller',
+      text: 'Das Leben ist entweder ein gewagtes Abenteuer oder nichts.'
+    },
+    {
+      image: 'assets/img-quotes/brecht.webp',
+      author: 'Bertolt Brecht',
+      text: 'Wer kämpft, kann verlieren. Wer nicht kämpft, hat schon verloren.'
+    },
+    {
+      image: 'assets/img-quotes/eliot.png',
+      author: 'George Eliot',
+      text: 'Es ist nie zu spät, das zu werden, was man hätte sein können.'
+    },
+    {
+      image: 'assets/img-quotes/disney.jpg',
+      author: 'Walt Disney',
+      text: 'Alle Träume können wahr werden, wenn wir den Mut haben, ihnen zu folgen.'
+    }
+  ];
 }
